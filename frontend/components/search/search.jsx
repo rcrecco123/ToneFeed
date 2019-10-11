2c49727b8eb076836d55f8ff2fd6d265fcad0e97
docdoom815 <ronald.crecco@gmail.com>
2019-10-11 02:11:56 -0400

    add filter logic

--- frontend/components/search/search.jsx
+++ frontend/components/search/search.jsx
@@ -33,32 +33,34 @@ class Search extends React.Component {
 
     render() {
         debugger
-        if (this.props.allTracks != undefined) {
+        // if (this.props.allTracks != undefined) {
 
-            let trackArr = Object.values(this.props.allTracks)
-            debugger
-            let filteredTracks = this.props.trackArr.filter(
-                track => {
-                    return track.name.toLowerCase()
-                        .indexOf(this.state.search.toLowerCase()) !== -1;
-                }
-            );
+        //     let trackArr = Object.values(this.props.allTracks)
+        //     debugger
+        //     let filteredTracks = this.props.trackArr.filter(
+        //         track => {
+        //             return track.name.toLowerCase()
+        //                 .indexOf(this.state.search.toLowerCase()) !== -1;
+        //         }
+        //     );
 
-        }
+        // }
 
 
         return (
             <div>
-                <ul>
-                    {filteredTracks.map(track => {
-                        return <li>{track.title}</li>
-                    })}
-                </ul>
                 <div>
                     <button className="fas fa-search" onClick={() => { this.props.search(this.state.searchString) }}></button>
                     <input className="nav-bar-search-bar" onChange={this.handleChange} name="search" />
 
                 </div>
+                <ul className="search-list">
+                    {Object.values(this.props.allTracks).map(track => {
+                        if (track.title.includes(`${this.state.searchString}`)) {
+                            return <li key={`${track.title.toString()}`}>{track.title}</li>
+                        }
+                    })}
+                </ul>
             </div>
         )
     }
