9ed33568828533db97979f669c109b946ee54c4f
docdoom815 <ronald.crecco@gmail.com>
2019-12-01 13:09:34 -0500

    update links

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
         <div className='footer-main'>
             <ul className="ul-footer">
                 <li className="li-footer"><a className="footer-a-tag" href="https://tonefeed.herokuapp.com/#/">Home</a></li>
-                <li className="li-footer"><a className="footer-a-tag" href="rcrecco123.github.io">Personal Site</a></li>
+                <li className="li-footer"><a className="footer-a-tag" href="https://rcrecco123.github.io">Personal Site</a></li>
                 <li className="li-footer"><a className="footer-a-tag" href="https://www.linkedin.com/in/ronald-crecco-2a4065136/">LinkedIn</a></li>
                 <li className="li-footer"><a className="footer-a-tag" href="mailto:ronald.crecco@gmail.com">Email</a></li>
             </ul>
